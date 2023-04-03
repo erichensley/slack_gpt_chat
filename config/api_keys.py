775d36dd55937e2f25659a9278b7173f74ed075f
erichensley <eric.hensley@gmail.com>
2023-04-03 11:13:17 -0700

    Delete api_keys.py

--- config/api_keys.py
+++
@@ -1,8 +0,0 @@
-openai_api_key = "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"
-openai_model_engine = "text-davinci-003"
-pinecone_api_key = "e93a8805-7d82-4afa-8033-20119ea31c76"
-pinecone_enviroment = "us-east1-gcp"
-slack_app_token = "xapp-1-A04DR3BU8JG-4458057500342-3e53011e7fd36368abbc6f1c0b158ed0a6a495cef60579b99f49c48fc9223ffd"
-slack_bot_token = "xoxb-4165207855-4458066408998-Rvof1vpFSxCy9HyLQYEVJZr1"
-
-
